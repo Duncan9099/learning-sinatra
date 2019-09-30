b7f0cdac7f07940ee01c0dc55f07d047c824f4b8
Duncan Skinner <duncanskinner94@gmail.com>
2019-09-30 15:52:37 +0100

    first commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+ruby '2.2.3'
+sinatra '2.0.7'
+shotgun '0.9.2'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
---
+++ myapp.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "This is a new route"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  <div>"
+end
